5d554dcca63a26c98b733cc7f05f4479794a249e
Chris <cmk0289@gmail.com>
2020-07-29 01:06:26 +0900

    add config

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const bodyParser = require('body-parser')
+const { User } = require('./models/user')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dbuser:dbuser@youtubetest.puz35.mongodb.net/test?retryWrites=true&w=majority', {
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!!!!!'))
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
